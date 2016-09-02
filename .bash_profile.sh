c7bee25e9cb0ba5f297b96c0029a0bc116f97001
Kjeld Schumacher <k.schumacher@familie-redlich.de>
2016-09-02 14:30:51 +0200

    add example .bash_profile.sh script

---
+++ .bash_profile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# familie redlich docker variables
+# Linux
+#export BOILERPLATEROOT="/var/docker/boilerplate/docker-boilerplate"
+#export HOSTPROJECTROOT="/var/www/projects"
+#alias dfr="/var/docker/boilerplate/dfr.sh"
+
+# MacOS
+export BOILERPLATEROOT="/Users/$(whoami)/Docker/boilerplate/docker-boilerplate"
+export HOSTPROJECTROOT="/Users/$(whoami)/Projects"
+alias dfr="/Users/$(whoami)/Docker/boilerplate/dfr.sh"
