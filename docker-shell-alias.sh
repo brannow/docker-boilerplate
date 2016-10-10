a22e7e0d20f49b8f2dbd64f0bfe5ecf79f935831
Benjamin Rannow <b.rannow@familie-redlich.de>
2016-10-10 15:09:27 +0200

    fix some fancy stuff (cleanup)

--- docker-shell-alias.sh
+++ docker-shell-alias.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# auto complete script for fast shell access to your docker container
+# simply type docker-shell PROJECTNAME ... and press tab!
+# replace projectname with your projectname - project directory name
+
 _complete_docker_container_names() {
 	local cur=${COMP_WORDS[COMP_CWORD]}
         COMPREPLY=($(compgen -W "$(docker ps | awk '{if(NR>1) print $NF}')" -- $cur))
--- inject_docker_ip.sh
+++ inject_docker_ip.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# add this script in the login hook
+# MAC ONLY
+# defaults write com.apple.loginwindow LoginHook /Users/benjamin/Docker/docker-boilerplate/inject_docker_ip.sh
+# edit the path to the script location
+
+# you can additionaly add a fancy hostname e.g. 'docker' as sample hostname in your /etc/hosts
+#172.0.0.1    docker
+
+# then you can call all your docker container with the hostname 'docker' without any ip and ports
+
+
 echo "
 rdr pass on lo0 inet proto tcp from 172.0.0.1 to any port = 80 -> 127.0.0.1 port 8001
 rdr pass on lo0 inet proto tcp from 172.0.0.1 to any port = 443 -> 127.0.0.1 port 8443
@@ -7,6 +18,5 @@ rdr pass on lo0 inet proto tcp from 172.0.0.1 to any port = 443 -> 127.0.0.1 por
 
 #sudo pfctl -s nat
 #sudo pfctl -F all -f /etc/pf.conf
-
 
 ifconfig lo0 alias 172.0.0.1
